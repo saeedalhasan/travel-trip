f1ab797222b2a290ed3013ca17571f1df54c9ca1
saeedalhasan <saeedalhasan619@gmail.com>
2022-11-06 22:17:23 +0600

    second commit

--- index.php
+++ index.php
@@ -18,12 +18,15 @@ if(isset($_POST['name'])){
 
     // Collect post variables
     $name = $_POST['name'];
-    $gender = $_POST['gender'];
     $age = $_POST['age'];
+    $occupation = $_POST['occupation'];
+    $gender = $_POST['gender'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
+    $service = $_POST['service'];
     $other = $_POST['other'];
-    $sql = "INSERT INTO `trip`.`tripp` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$other', current_timestamp());";
+    $sql = "INSERT INTO `trip`.`drip` (`name`, `age`, `occupation`, `gender`, `email`, `phone`, `service`, `other`, `dt`) VALUES ('$name', '$age', '$occupation', '$gender', '$email', '$phone', '$service', '$other', current_timestamp());";
+    
     
 
     // Execute the query
@@ -57,15 +60,28 @@ if(isset($_POST['name'])){
             <p>Enter your details and submit this form to confirm your participation in the trip </p>
             <?php
         if($insert == true){
-        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        echo "<p class='submitMsg'>Thanks for submitting your form. We will contact you soon.</p>";
         }
     ?>
             <form action="index.php" method="post">
                 <input type="text" name="name" id="name" placeholder="Enter your name">
                 <input type="text" name="age" id="age" placeholder="Enter your Age">
-                <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+                <input type="text" name="occupation" id="occupation" placeholder="Enter your designation">
+                <select id="gender" name="gender">
+                    <option value="0"><span id="selection">Select Your Gender ...</span> </option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                </select>
                 <input type="email" name="email" id="email" placeholder="Enter your email">
                 <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+                <select id="service" name="service">
+                    <option value="0"><span id="selection">Service you want ... </span></option>
+                    <option value="room">Room</option>
+                    <option value="kitchen">Kitchen</option>
+                    <option value="parking">Parking</option>
+                    <option value="guide">Tour guide</option>
+                </select>
                 <textarea name="other" id="other" cols="30" rows="10"
                     placeholder="Enter any other information here"></textarea>
                 <button class="btn">Submit</button>
